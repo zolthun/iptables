7b4e650221e99222ebe53bbae63d16c9ee8f8e8e
Zolthun <zavalsa@gmail.com>
2019-04-16 11:14:48 +0200

    Update ssh_to_iptables.sh
    Added -z testing for $aux, when there aren't any lines to be added to the iptables list, the system will warn you.

--- ssh_to_iptables.sh
+++ ssh_to_iptables.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+#### Default system log folder
 log_dir="/var/log/"
+#### Usually the ssh attemps are stored in the files auth.log, auth.1.log and so on
 pattern="auth*.*"
 tmp_log_files=`ls $log_dir | grep -i $pattern`
 my_ip=`w | grep -i "root" | awk -F " " '{ print $3 }' | sort -u`
-let ban_count=`iptables -L INPUT -v -n | grep -i DROP | grep -i "$k" | wc -l`
 for i in $tmp_log_files
 do
         log_files=$log_dir$i
@@ -12,23 +13,27 @@ do
         do
                 aux_auth=`cat $j | grep -i "closed by" | grep -v "exceeded" | grep -v $my_ip | awk -F " " '{ print $9 }'`
                 aux_tmp=`iptables -L INPUT -v -n | grep -i DROP | awk -F " " '{ print $8 }'`
-                aux=`diff -ia --suppress-common-lines <( echo "$aux_tmp" |  tr ' ' '\n' | sort -u ) <( echo "$aux_auth" |  tr ' ' '\n' | sort -u ) | grep -E "<|>" | awk -F" " '{ print $2 }'`
         done
+                aux=`diff -ia --suppress-common-lines <( echo "$aux_tmp" |  tr ' ' '\n' | sort -u ) <( echo "$aux_auth" |  tr ' ' '\n' | sort -u ) | grep -E "<|>" | awk -F" " '{ print $2 }'`
+
 done
 
-echo $aux |  tr ' ' '\n' | sort -u
-
-for k in $aux
-do
-        if [ "$ban_count" -gt "0" ]
-        then
-                echo "The ip address $k has been already banned,nothing done."
-        else
-                echo "Ban to ip address $k."
-                iptables -A INPUT -s $k -j DROP
-                echo $k >> banned
-        fi
-done
+if [ -z "$aux" ]
+then
+        echo "There are no lines to be added to the drop rules."
+else
+        for k in $aux
+        do
+                let ban_count=`iptables -L INPUT -v -n | grep -i DROP | grep -i "$k" | wc -l`
+                if [ "$ban_count" -gt "0" ]
+                then
+                        echo "The ip address $k has been already banned,nothing done"
+                else
+                        echo "Ban to ip address $k"
+                        iptables -A INPUT -s $k -j DROP
+                fi
+        done
+fi
 
 
 #       Zolthun 2019
